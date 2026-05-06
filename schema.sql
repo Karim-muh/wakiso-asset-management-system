dda4c0a7766f90298214981f8c1fe0bf57e92694
Karim-muh <muhsinlugemwa1@gmail.com>
2026-05-06 12:00:55 +0300

    updatedschema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,46 @@
-CREATE DATABASE asset_db;
+# USERS
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) UNIQUE,
+    email VARCHAR(35),
+    password VARCHAR(40),
+    role ENUM('admin', 'employee') NOT NULL
+);
 
-USE asset_db;
+# DEPARTMENTS
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    location VARCHAR(100)
+);
 
+# ASSETS
 CREATE TABLE assets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
-    category VARCHAR(50)
+    category VARCHAR(50),
+    status ENUM('available','assigned','maintenance','disposed') DEFAULT 'available',
+    location VARCHAR(100)
+);
+
+# REQUISITIONS (REQUESTS)
+CREATE TABLE requisitions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    asset_id INT,
+    status ENUM('pending','approved','rejected') DEFAULT 'pending',
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (asset_id) REFERENCES assets(id)
+);
+
+# ALLOCATIONS (ASSIGNMENT)
+CREATE TABLE allocations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    asset_id INT,
+    user_id INT,
+    issue_date DATE,
+    return_date DATE,
+    FOREIGN KEY (asset_id) REFERENCES assets(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
